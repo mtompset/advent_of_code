f6a8f382ccf91fa01a2f2bf92faeb17a1649fa6b
Mark Tompsett <mtompset@hotmail.com>
2021-12-01 16:02:09 -0500

    Perlcritic clean up on day5a

--- 2015/day5/day5a.pl
+++ 2015/day5/day5a.pl
@@ -1,7 +1,11 @@
 #!/usr/bin/perl -Tw
 
 use Modern::Perl;
-use Data::Dumper;
+use Const::Fast;
+
+our $VERSION = '1.0';
+
+const my $NUMBER_OF_VOWELS => 3;
 
 my @input = <>;
 my @words;
@@ -10,13 +14,10 @@ foreach my $line (@input) {
     push @words, $line;
 }
 
-my $naughty_count = 0;
+my $naughty_count   = 0;
 my $number_of_words = scalar @words;
 for my $word (@words) {
-    print "$word -- ", is_naughty($word), "\n";
-    print "*******\n";
     $naughty_count += is_naughty($word);
-    print "*******\n";
 }
 my $nice_count = $number_of_words - $naughty_count;
 print "There are $nice_count nice words.\n";
@@ -24,35 +25,29 @@ print "There are $nice_count nice words.\n";
 sub is_naughty {
     my ($word) = @_;
 
-    if ($word =~ /ab/gxlsm) {
-        print "AB ";
+    if ( $word =~ /ab/gxlsm ) {
         return 1;
     }
-    if ($word =~ /cd/gxlsm) {
-        print "CD ";
+    if ( $word =~ /cd/gxlsm ) {
         return 1;
     }
-    if ($word =~ /pq/gxlsm) {
-        print "PQ ";
+    if ( $word =~ /pq/gxlsm ) {
         return 1;
     }
-    if ($word =~ /xy/gxlsm) {
-        print "XY ";
+    if ( $word =~ /xy/gxlsm ) {
         return 1;
     }
-    if (has_three_vowels($word) == 0) {
-        print "3V ";
+    if ( has_three_vowels($word) == 0 ) {
         return 1;
     }
-    my @characters = split //, $word;
-    for my $position (1 .. scalar @characters - 1) {
-        my $character = $characters[$position - 1];
+    my @characters = split //xlsm, $word;
+    for my $position ( 1 .. scalar @characters - 1 ) {
+        my $character      = $characters[ $position - 1 ];
         my $next_character = $characters[$position];
-        if ($character eq $next_character) {
+        if ( $character eq $next_character ) {
             return 0;
         }
     }
-    print "XX ";
     return 1;
 }
 
@@ -62,10 +57,10 @@ sub has_three_vowels {
     my $before_length = length $word;
     $word =~ s/[aeiou]//gxslm;
     my $after_length = length $word;
-    print $before_length, "###", $after_length, "\n";
-    if ($before_length - $after_length >= 3) {
+    if ( $before_length - $after_length >= $NUMBER_OF_VOWELS ) {
         return 1;
-    } else {
+    }
+    else {
         return 0;
     }
 }
