2b0049bf6fcac732c12be06651564c9ab1478da9
Mark Tompsett <mtompset@hotmail.com>
2021-12-01 22:52:46 -0500

    Perlcritic clean up

--- 2015/day6/day6a.pl
+++ 2015/day6/day6a.pl
@@ -6,30 +6,35 @@ use Const::Fast;
 use Data::Dumper;
 
 const my $TOGGLE_COMMAND => -1;
-const my $ON_COMMAND => 1;
-const my $OFF_COMMAND => 0;
+const my $ON_COMMAND     => 1;
+const my $OFF_COMMAND    => 0;
+const my $UPPER_BOUNDARY => 999;
+
+our $VERSION = '1.0';
 
 my @instructions = <>;
 my %light;
 foreach my $instruction (@instructions) {
-    chomp $instruction; # deal with line-feed issues
-    if ($instruction =~ /([^\d]*) (\d*),(\d*) .*? (\d*),(\d*)/xlsm) {
-        my $command   = trim($1);
-        my $x_start   = int(trim($2));
-        my $y_start   = int(trim($3));
-        my $x_end     = int(trim($4));
-        my $y_end     = int(trim($5));
-        my $set_value = (lc $command =~ /toggle/) ? $TOGGLE_COMMAND :
-                        (lc $command =~ /turn on/) ? $ON_COMMAND :
-                        $OFF_COMMAND;
+    chomp $instruction;    # deal with line-feed issues
+    if ( $instruction =~ /([^\d]*) (\d*),(\d*) .*? (\d*),(\d*)/xlsm ) {
+        my $command = trim($1);
+        my $x_start = int trim($2);
+        my $y_start = int trim($3);
+        my $x_end   = int trim($4);
+        my $y_end   = int trim($5);
+        my $set_value =
+            ( lc $command =~ /toggle/xlsm )  ? $TOGGLE_COMMAND
+          : ( lc $command =~ /turn on/xlsm ) ? $ON_COMMAND
+          :                                    $OFF_COMMAND;
         for my $x_position ( $x_start .. $x_end ) {
             for my $y_position ( $y_start .. $y_end ) {
-                my $position = sprintf "%d,%d", $x_position, $y_position;
+                my $position      = sprintf '%d,%d', $x_position, $y_position;
                 my $initial_value = $light{$position} // 0;
                 my $new_value;
-                if ($set_value == $TOGGLE_COMMAND) {
+                if ( $set_value == $TOGGLE_COMMAND ) {
                     $new_value = $initial_value == 1 ? 0 : 1;
-                } else {
+                }
+                else {
                     $new_value = $set_value;
                 }
                 $light{$position} = $new_value;
@@ -39,10 +44,10 @@ foreach my $instruction (@instructions) {
 }
 
 my $number_lit = 0;
-for my $x (0..999) {
-    for my $y (0..999) {
-        my $position = sprintf "%d,%d", $x, $y;
-        my $value = $light{$position} // 0;
+for my $x ( 0 .. $UPPER_BOUNDARY ) {
+    for my $y ( 0 .. $UPPER_BOUNDARY ) {
+        my $position = sprintf '%d,%d', $x, $y;
+        my $value    = $light{$position} // 0;
         $number_lit += $value;
     }
 }
--- 2015/day6/day6b.pl
+++ 2015/day6/day6b.pl
@@ -5,29 +5,33 @@ use Text::Trim;
 use Const::Fast;
 use Data::Dumper;
 
+our $VERSION = '1.0';
+
 const my $TOGGLE_COMMAND => 2;
-const my $ON_COMMAND => 1;
-const my $OFF_COMMAND => -1;
+const my $ON_COMMAND     => 1;
+const my $OFF_COMMAND    => -1;
+const my $UPPER_BOUNDARY => 999;
 
 my @instructions = <>;
 my %light;
 foreach my $instruction (@instructions) {
-    chomp $instruction; # deal with line-feed issues
-    if ($instruction =~ /([^\d]*) (\d*),(\d*) .*? (\d*),(\d*)/xlsm) {
-        my $command   = trim($1);
-        my $x_start   = int(trim($2));
-        my $y_start   = int(trim($3));
-        my $x_end     = int(trim($4));
-        my $y_end     = int(trim($5));
-        my $set_value = (lc $command =~ /toggle/) ? $TOGGLE_COMMAND :
-                        (lc $command =~ /turn on/) ? $ON_COMMAND :
-                        $OFF_COMMAND;
+    chomp $instruction;    # deal with line-feed issues
+    if ( $instruction =~ /([^\d]*) (\d*),(\d*) .*? (\d*),(\d*)/xlsm ) {
+        my $command = trim($1);
+        my $x_start = int trim($2);
+        my $y_start = int trim($3);
+        my $x_end   = int trim($4);
+        my $y_end   = int trim($5);
+        my $set_value =
+            ( lc $command =~ /toggle/xlsm )  ? $TOGGLE_COMMAND
+          : ( lc $command =~ /turn on/xlsm ) ? $ON_COMMAND
+          :                                    $OFF_COMMAND;
         for my $x_position ( $x_start .. $x_end ) {
             for my $y_position ( $y_start .. $y_end ) {
-                my $position = sprintf "%d,%d", $x_position, $y_position;
+                my $position      = sprintf '%d,%d', $x_position, $y_position;
                 my $initial_value = $light{$position} // 0;
-                my $new_value = $initial_value + $set_value;
-                $new_value = ($new_value < 0) ? 0 : $new_value;
+                my $new_value     = $initial_value + $set_value;
+                $new_value = ( $new_value < 0 ) ? 0 : $new_value;
                 $light{$position} = $new_value;
             }
         }
@@ -35,10 +39,10 @@ foreach my $instruction (@instructions) {
 }
 
 my $brightness = 0;
-for my $x (0..999) {
-    for my $y (0..999) {
-        my $position = sprintf "%d,%d", $x, $y;
-        my $value = $light{$position} // 0;
+for my $x ( 0 .. $UPPER_BOUNDARY ) {
+    for my $y ( 0 .. $UPPER_BOUNDARY ) {
+        my $position = sprintf '%d,%d', $x, $y;
+        my $value    = $light{$position} // 0;
         $brightness += $value;
     }
 }
