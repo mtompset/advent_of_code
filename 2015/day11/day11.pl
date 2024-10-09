39a3249314598e2cf37cf592cf9a5c29a3bd44b8
Mark Tompsett <mtompset@hotmail.com>
2024-10-09 13:18:38 -0400

    Perlcritic -2 day11

--- 2015/day11/day11.pl
+++ 2015/day11/day11.pl
@@ -3,9 +3,13 @@
 use Modern::Perl;
 use Data::Dumper;
 use Math::Combinatorics;
+use English qw( -no_match_vars );
+use Const::Fast;
 
+const my $LAST_LETTER_ORDINAL => 26;
 our $VERSION = '1.0';
-$| = 1;
+local $OUTPUT_AUTOFLUSH = 1;
+
 
 my $old_password = <>;
 chomp $old_password;
@@ -14,8 +18,8 @@ print "Starting with $old_password\n";
 my $new_password;
 while ($new_password = calculate_next($old_password)) {
     if (is_password_okay($new_password)) {
-        print "\n";
-        die "NEW PASSWORD: $new_password\n";
+        print "\nNEW PASSWORD: $new_password\n";
+        last;
     } else {
         print "$new_password\n";
         $old_password = $new_password;
@@ -27,17 +31,17 @@ sub calculate_next {
     my ($string) = @_;
 
     my @data = split //xslm, $string;
-    for (my $count = 0; $count <= $#data; ++$count) {
+    foreach my $count (0..$#data) {
         $data[$count] = ord($data[$count]) - ord('a');
     }
     $data[$#data] += 1;
-    for (my $count = $#data; $count >= 0; --$count) {
-        if ($data[$count] >= 26) {
+    foreach my $count (reverse 0..$#data) {
+        if ($data[$count] >= $LAST_LETTER_ORDINAL) {
             $data[$count - 1] += 1;
             $data[$count] = 0;
         }
     }
-    for (my $count = 0; $count <= $#data; ++$count) {
+    foreach my $count (0..$#data) {
         $data[$count] = chr($data[$count] + ord('a'));
     }
     return join q{}, @data;
@@ -57,7 +61,7 @@ sub has_three_in_a_row {
     my ($string) = @_;
 
     my @data = split //xslm, $string;
-    for (my $count = 0; $count <= $#data - 2; ++$count) {
+    foreach my $count (0..($#data - 2)) {
         my $c1 = $data[$count + 0];
         my $c2 = $data[$count + 1];
         my $c3 = $data[$count + 2];
@@ -72,7 +76,7 @@ sub has_no_iol {
     my ($string) = @_;
 
     my $stripped = $string;
-    $stripped =~ s/[iol]//g;
+    $stripped =~ s/[iol]//gmxls;
 
     if (length $stripped == length $string) {
         return 1;
@@ -87,16 +91,18 @@ sub has_two_different_nonoverlapping_pairs {
     my @data = split //xslm, $string;
     my $pair1 = q{};
     my $pair2 = q{};
-    for (my $count = 0; $count <= $#data - 1; ++$count) {
+    # Find first pair
+    foreach my $count (0..($#data-1)) {
         my $c1 = $data[$count + 0];
         my $c2 = $data[$count + 1];
         if ($c1 eq $c2) {
             $pair1 = $data[$count];
         }
     }
-    for (my $count = $#data; $count > 0; --$count) {
-        my $c1 = $data[$count - 0];
-        my $c2 = $data[$count - 1];
+    # Find last pair
+    foreach my $count (reverse 0..($#data-1)) {
+        my $c1 = $data[$count + 0];
+        my $c2 = $data[$count + 1];
         if ($c1 eq $c2 && $c1 ne $pair1) {
             $pair2 = $data[$count];
         }
