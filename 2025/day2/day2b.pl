f13471e83e9c8eefb68d084f3ed127c583933622
Mark Tompsett <mtompset@hotmail.com>
2025-12-02 01:06:54 -0500

    Day 2 for 2025
    Tried to keep it perlcritic -1 clean as much as possible.

---
+++ 2025/day2/day2b.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl -Tw
+
+use Modern::Perl;
+use Text::Trim;
+use List::Util qw(sum0);
+use Data::Dumper;
+
+our $VERSION = '1.0';
+
+my $input = <>;
+chomp $input;
+my @ranges = split /,/xlsm, $input;
+my @all_invalid_ids;
+foreach my $range (@ranges) {
+    my @invalid_ids = get_invalid_ids($range);
+    push @all_invalid_ids, @invalid_ids;
+}
+my @ids               = map { int } @all_invalid_ids;
+my $total_invalid_ids = sum0(@ids);
+print "The sum of invalid ids is: $total_invalid_ids\n";
+
+sub get_invalid_ids {
+    my ($string_range) = @_;
+    my @range          = split /-/xlsm, $string_range;
+    my @range_ids      = map { int } @range;
+    my @invalid;
+
+    foreach my $id ( $range_ids[0] .. $range_ids[1] ) {
+        my $string_id   = sprintf '%s', $id;
+        my $full_length = length $string_id;
+        foreach my $count ( 1 .. $full_length - 1 ) {
+            if ( $full_length % $count == 0 ) {
+                my @parts  = split_string( $string_id, $count );
+                my %string = map { $_ => 1 } @parts;
+                if ( keys %string == 1 ) {
+                    push @invalid, $string_id;
+                    last;
+                }
+            }
+        }
+    }
+    return @invalid;
+}
+
+sub split_string {
+    my ( $string_to_split, $split_size ) = @_;
+
+    my @splits;
+    my $index = 0;
+    while ( $index < length $string_to_split ) {
+        push @splits, substr $string_to_split, $index, $split_size;
+        $index += $split_size;
+    }
+    return @splits;
+}
